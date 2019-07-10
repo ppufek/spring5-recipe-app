6b8577e5a95a197b352856c0c14d3c5524ea41e3
epufpau <paula.pufek@ericsson.com>
2019-07-10 11:48:17 +0200

    additional misssing files

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,15 +1,29 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Created by jt on 6/1/17.
+ */
+@Slf4j
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
+    private final RecipeService recipeService;
 
-        System.out.println("some message to say... 321");
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        log.debug("Getting Index page");
+
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
