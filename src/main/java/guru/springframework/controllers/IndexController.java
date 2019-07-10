082200def837cfc5d1a9eb9ece23a22ceaa57c65
epufpau <paula.pufek@ericsson.com>
2019-07-10 11:41:32 +0200

    adding the project to Git

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+
+        System.out.println("some message to say... 321");
+
+        return "index";
+    }
+}
